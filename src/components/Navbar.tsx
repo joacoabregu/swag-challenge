9e309fc9e577b80cfb03d51060500c0f17bcfd6b
Joaquin Abregu <joaquin.abregu@estoes.me>
2023-01-22 16:41:50 -0300

    add navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,40 +1,46 @@
 /** @jsxImportSource @emotion/react */
 import { Container } from '@mui/material';
-
 import { css } from '@emotion/react';
 import { backgroundGray, textGray } from '../styles';
 
-
 const nav = css`
-    background-color: ${backgroundGray};
-    padding-top: 24px;
-    padding-bottom: 24px;
-`
+  background-color: ${backgroundGray};
+  padding-top: 24px;
+  padding-bottom: 24px;
+`;
 
 const container = css`
-    display: flex;
-    flex-direction: row;
-    > a  {
-        color: ${textGray};
-        font-size: 15px;
-        cursor: pointer;
-       & + a { 
-        margin-left: 24px;
-        }
+  display: flex;
+  flex-direction: row;
+  > a {
+    color: ${textGray};
+    font-size: 15px;
+    cursor: pointer;
+    & + a {
+      margin-left: 24px;
     }
-`
+  }
+`;
 
-const navLinks = ['All products', 'Packaging', 'Drinkware', 'Apparel', 'Notebooks', 'Backpacks']
+const navLinks = [
+  'All products',
+  'Packaging',
+  'Drinkware',
+  'Apparel',
+  'Notebooks',
+  'Backpacks',
+];
 
 const Navbar = () => {
-    return (
-        <nav css={nav}>
-            <Container css={container} maxWidth='xl'>
-                {navLinks.map((link) => <a>{link}</a>)}
-            </Container>
+  return (
+    <nav css={nav}>
+      <Container css={container} maxWidth='xl'>
+        {navLinks.map((link) => (
+          <a>{link}</a>
+        ))}
+      </Container>
+    </nav>
+  );
+};
 
-        </nav>
-    )
-}
-
-export default Navbar
+export default Navbar;
