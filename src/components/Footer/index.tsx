29c8f8fa0dcd4ad6e756f15330f98bee77b7a613
Joaquin Abregu <joaquin.abregu@estoes.me>
2023-01-22 20:57:23 -0300

    add Links and ContactInfo

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,10 +1,10 @@
 /** @jsxImportSource @emotion/react */
 import { css } from '@emotion/react';
-import { Container, SelectChangeEvent, Select, MenuItem } from '@mui/material';
-import { useState } from 'react';
-
+import { Container } from '@mui/material';
 import { backgroundGray } from '../../styles';
+import ContactInfo from './ContactInfo';
 import Copyright from './Copyright';
+import Links from './Links';
 import Selectors from './Selectors';
 
 const footer = css`
@@ -12,19 +12,28 @@ const footer = css`
 `;
 
 const container = css`
-    > div {
-        display: flex;
-        flex-direction: row;
-        justify-content: space-between;
-    }
-`
+  > div:nth-of-type(1) {
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);
+    column-gap: 24px;
+    margin-bottom: 112px;
+  }
+  > div:nth-of-type(2) {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+  }
+`;
 
 const Footer = () => {
-  
-
   return (
     <footer css={footer}>
       <Container css={container} maxWidth='xl'>
+        <div>
+          <ContactInfo />
+          <Links />
+        </div>
         <div>
           <Copyright />
           <Selectors />
