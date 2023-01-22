a6aa6987f8a3d5a3e004cda4aee29abec14ebf15
Joaquin Abregu <joaquin.abregu@estoes.me>
2023-01-22 20:57:35 -0300

    add dark03 color

--- src/styles/index.ts
+++ src/styles/index.ts
@@ -2,5 +2,6 @@
 export const backgroundGray = '#E6E8E9'
 export const textGray = '#535C67'
 export const dark01 = '#091625'
+export const dark03 = '#3A4451'
 export const dark04 = '#535C67'
 export const dark05 = '#6B737C'
