80dcb4c6666fee8f7429f78a628c26fb738febf7
Joaquin Abregu <joaquin.abregu@estoes.me>
2023-01-22 16:41:41 -0300

    add navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import Header from './components/Header'
+import Navbar from './components/Navbar'
 
 function App() {
   
   return (
     <div className="App">
       <Header />
+      <Navbar />
     </div>
   )
 }
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,40 @@
+/** @jsxImportSource @emotion/react */
+import { Container } from '@mui/material';
+
+import { css } from '@emotion/react';
+import { backgroundGray, textGray } from '../styles';
+
+
+const nav = css`
+    background-color: ${backgroundGray};
+    padding-top: 24px;
+    padding-bottom: 24px;
+`
+
+const container = css`
+    display: flex;
+    flex-direction: row;
+    > a  {
+        color: ${textGray};
+        font-size: 15px;
+        cursor: pointer;
+       & + a { 
+        margin-left: 24px;
+        }
+    }
+`
+
+const navLinks = ['All products', 'Packaging', 'Drinkware', 'Apparel', 'Notebooks', 'Backpacks']
+
+const Navbar = () => {
+    return (
+        <nav css={nav}>
+            <Container css={container} maxWidth='xl'>
+                {navLinks.map((link) => <a>{link}</a>)}
+            </Container>
+
+        </nav>
+    )
+}
+
+export default Navbar
