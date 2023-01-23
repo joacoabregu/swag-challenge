265df73dac5356c5f4cd7fcd8e3afe2e77b3d40a
Joaquin Abregu <joaquin.abregu@estoes.me>
2023-01-23 19:13:56 -0300

    add recommendedProducts styles

--- src/components/RecommendedProducts/index.tsx
+++ src/components/RecommendedProducts/index.tsx
@@ -2,42 +2,45 @@
 import { css } from '@emotion/react';
 import { Container } from '@mui/material';
 import ProductCard from './ProductCard';
+import ProductPhoto from '../../assets/Photo.png'
 
 const container = css`
+margin-bottom: 120px;
+  > p {
+    font-size: 24px;
+    font-weight: 700;
+    margin-bottom: 24px;
+  }
   > div {
     display: grid;
     grid-template-columns: repeat(4, 1fr);
+    grid-template-rows: auto;
     column-gap: 24px;
+    row-gap: 24px;
   }
 `;
 
 const products = [
   {
-    imgURL: '',
+    imgURL: ProductPhoto,
     name: 'Unisex Short Sleeve T-Shirt',
     price: '$10 - $24',
     quantity: '24',
   },
   {
-    imgURL: '',
+    imgURL: ProductPhoto,
     name: 'Unisex Short Sleeve T-Shirt',
     price: '$10 - $24',
     quantity: '24',
   },
   {
-    imgURL: '',
+    imgURL: ProductPhoto,
     name: 'Unisex Short Sleeve T-Shirt',
     price: '$10 - $24',
     quantity: '24',
   },
   {
-    imgURL: '',
-    name: 'Unisex Short Sleeve T-Shirt',
-    price: '$10 - $24',
-    quantity: '24',
-  },
-  {
-    imgURL: '',
+    imgURL: ProductPhoto,
     name: 'Unisex Short Sleeve T-Shirt',
     price: '$10 - $24',
     quantity: '24',
