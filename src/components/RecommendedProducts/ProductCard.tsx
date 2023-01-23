54345bcf18dadc0c9c2134b16a3e2c701198d5f3
Joaquin Abregu <joaquin.abregu@estoes.me>
2023-01-23 19:13:06 -0300

    add Product Card styles

--- src/components/RecommendedProducts/ProductCard.tsx
+++ src/components/RecommendedProducts/ProductCard.tsx
@@ -1,5 +1,37 @@
 /** @jsxImportSource @emotion/react */
 import { css } from '@emotion/react';
+import { dark05 } from '../../styles';
+
+const container = css`
+  > img {
+    width: 100%;
+    margin-bottom: 16px;
+  }
+  // Product Name
+  > p {
+    font-weight: 700;
+    margin-bottom: 10px;
+  }
+  > div {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    // Price
+    > p:nth-of-type(1){
+      font-size: 15px;
+      font-weight: 600;
+    }
+    // Quantity
+    > p:nth-of-type(2) {
+      color: ${dark05};
+      font-size: 14px;
+      span {
+        color: black;
+      }
+    }
+
+  }
+`
 
 interface Props {
   name: string;
@@ -10,12 +42,12 @@ interface Props {
 
 const ProductCard = ({ name, imgURL, price, quantity }: Props) => {
   return (
-    <div key={name}>
+    <div css={container}>
       <img src={imgURL} />
       <p>{name}</p>
       <div>
         <p>{price} </p>
-        <p> Minimum: {quantity} </p>
+        <p> Minimum: <span>{quantity}</span></p>
       </div>
     </div>
   );
