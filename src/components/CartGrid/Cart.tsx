84c2843a4041d085087a3be821c22e70f7f19b90
Joaquin Abregu <joaquin.abregu@estoes.me>
2023-01-26 09:06:44 -0300

    refactor components

--- src/components/CartGrid/Cart.tsx
+++ src/components/CartGrid/Cart.tsx
@@ -1,10 +1,9 @@
 /** @jsxImportSource @emotion/react */
 import { css } from '@emotion/react';
 import ProductCard from './ProductCard';
-import DeleteOutlineOutlinedIcon from '@mui/icons-material/DeleteOutlineOutlined';
-import { Button } from '@mui/material';
-import { dark01, dark05 } from '../../styles';
 import type { Cart as ICart, Action } from '../../state';
+import ButtonRemoveProduct from './ButtonRemoveProduct';
+import PackContent from './PackContent'
 
 const container = css`
   grid-column-start: 1;
@@ -17,89 +16,6 @@ const container = css`
   }
 `;
 
-const btnStyle = css`
-  padding: 0;
-  color: black;
-  font-size: 12px;
-  text-transform: none;
-  position: relative;
-  // Icon
-  svg {
-    position: absolute;
-    left: -3px;
-  }
-  // Btn Text
-  span {
-    position: absolute;
-    left: 22px;
-    font-size: 12px;
-    font-weight: 600;
-    font-family: 'Open Sans';
-  }
-`;
-
-interface ButtonRemoveProps {
-  onClickHandler: (name: string) => void;
-  name: string;
-}
-function ButtonRemoveProduct({ onClickHandler, name }: ButtonRemoveProps) {
-  return (
-    <Button css={btnStyle} onClick={() => onClickHandler(name)}>
-      <DeleteOutlineOutlinedIcon fontSize='small' /> <span>Remove</span>
-    </Button>
-  );
-}
-
-const packContentstyle = css`
-  ul {
-    margin-left: 4px;
-    padding-left: 20px;
-    li {
-      color: ${dark01};
-      font-size: 14px;
-      font-weight: 600;
-      > span {
-        color: ${dark05};
-      }
-    }
-  }
-  button {
-    font-size: 12px;
-    font-weight: 600;
-    text-transform: none;
-    color: ${dark01};
-    padding: 0 4px 0 0;
-
-    min-width: fit-content;
-  }
-  // Btn divider
-  > span {
-    font-size: 12px;
-    color: ${dark05};
-  }
-`;
-
-function PackContent({ name, onClickHandler }: ButtonRemoveProps) {
-  return (
-    <div css={packContentstyle}>
-      <ul>
-        <li>
-          Cardboard box: <span>(container)</span>
-        </li>
-        <li>
-          Unisex Short Sleeve T-Shirt <span>(Green, Small)</span>
-        </li>
-        <li>
-          Basic bottle: <span>(Blue)</span>
-        </li>
-      </ul>
-      <Button size='small'>Edit</Button> <span>|</span>{' '}
-      <Button size='small' onClick={() => onClickHandler(name)}>
-        Remove
-      </Button>
-    </div>
-  );
-}
 interface Props {
   cart: ICart;
   dispatch: React.Dispatch<Action>;
